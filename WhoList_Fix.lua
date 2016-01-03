07ccd926719160dd6461895eef66148ba0e7fde0
Voidmenull <thehedgehog@arcor.de>
2016-01-03 23:42:36 +0100

    fix

--- WhoList_Fix.lua
+++ WhoList_Fix.lua
@@ -3,16 +3,13 @@
 local OrigWhoList_Update = WhoList_Update
 
 function WhoList_Update()
-  if (not SWALLOW_NEXT_WHO) then
     OrigWhoList_Update()
 	local numWhos, totalCount = GetNumWhoResults()
 	local displayedText = ""
 	if numWhos >= 49 then 
-		displayedText = format(WHO_FRAME_SHOWN_TEMPLATE, numWhos)
 		WhoFrameTotals:SetText(format(GetText("WHO_FRAME_TOTAL_TEMPLATE", nil, totalCount), totalCount).."  ".."(49+ found)")
 	else
 		displayedText = format(WHO_FRAME_SHOWN_TEMPLATE, numWhos)
 		WhoFrameTotals:SetText(format(GetText("WHO_FRAME_TOTAL_TEMPLATE", nil, totalCount), totalCount).."  "..displayedText)
 	end
-  end
 end
